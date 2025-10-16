8dbd4af2f5d699f827f0cfee54a0d2fabb480462
justice0404 <wzw13579@qq.com>
2025-10-16 13:58:57 +0800

    Vector的一个实例

--- STL/useVector.cpp
+++ STL/useVector.cpp
@@ -3,8 +3,40 @@
 
 using namespace std;
 
+void test01(void)
+{
+    vector<int> arr(10,1);
+    arr.push_back(2);
+    arr.push_back(3);
+    arr.push_back(4);
+    arr.pop_back();
+    arr.resize(20,5);
+
+    arr.resize(15);
+    
+    vector<int>::iterator it = arr.begin();
+    while(it != arr.end())
+    {
+        cout << *it << endl;
+        it++;
+    }
+
+    cout << "---------------------分界线---------------------------" << endl;
+
+    vector<int>::reverse_iterator it_r = arr.rbegin();
+    while (it_r != arr.rend())
+    {
+        /* code */
+        cout << *it_r << endl;
+        it_r++;
+    }
+    
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
+    test01();
+    
     return 0;
 }
